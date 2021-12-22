5e9024207855c981c1b712888db6430e3f9714ca
GHAYku <kusa07122@gmail.com>
2021-12-22 10:45:25 +0900

    [Finish]一覧でページネーションを設定する

--- memo/index.php
+++ memo/index.php
@@ -1,9 +1,13 @@
 <?php
 require('db_connect.php');
-$memos = $db->query('select * from memos order by id desc');
-if(!$memos){
+$stmt = $db->prepare('select * from memos order by id desc limit ?, 5');
+if(!$stmt){
   die($db->error);
 }
+$page = filter_input(INPUT_GET,'page',FILTER_SANITIZE_NUMBER_INT);
+$start = ($page - 1) * 5;
+$stmt->bind_param('i', $start);
+$stmt->execute();
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -16,10 +20,11 @@ if(!$memos){
 <body>
   <h1>メモ帳</h1>
   <p>→<a href="input.html">新しいメモ</a></p>
-  <?php while($memo = $memos->fetch_assoc()): ?>
+  <?php $stmt->bind_result($id, $memo, $created); ?>
+  <?php while($stmt->fetch()): ?>
       <div>
-        <h2><a href="memo.php?id=<?php echo $memo['id']; ?>"><?php echo htmlspecialchars(mb_substr($memo['memo'],0,50)); ?></a></h2>
-        <time><?php echo htmlspecialchars($memo['created']); ?></time>
+        <h2><a href="memo.php?id=<?php echo $id; ?>"><?php echo htmlspecialchars(mb_substr($memo,0,50)); ?></a></h2>
+        <time><?php echo htmlspecialchars($created); ?></time>
       </div>
     <hr>
   <?php endwhile ?>
