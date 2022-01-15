eadd60a7c431bd3868aa0457eed050ef3d448b51
GHAYku <kusa07122@gmail.com>
2022-01-15 13:17:14 +0900

    [Finish]PHPDoc

--- fullstack-webdev/040_PHP基礎Part2/0900_PHPDocを書いてみよう/start/index.php
+++ fullstack-webdev/040_PHP基礎Part2/0900_PHPDocを書いてみよう/start/index.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * 税率計算のための関数を記述するためのファイル
+ * 
+ * @author kusanagi shinnnosuke
+ * @since 1.0.0
+ */
+
+ /**
+  * 税込み金額を取得する関数
+　*
+  * @param int $base_price 価格
+  * @param float $tax_rate 税率
+  * @return int
+  * @see https://.....
+  */
 function with_tax($base_price, $tax_rate = 0.1) {
     $sum_price = $base_price + ($base_price * $tax_rate);
     $sum_price = round($sum_price);
