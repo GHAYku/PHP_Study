67f829562f2baa8c844324dec816a9cab58be6dd
GHAYku <kusa07122@gmail.com>
2022-01-16 20:35:08 +0900

    [Finish]クラス継承

--- fullstack-webdev/050_PHP基礎Part3/1000_クラス継承（Part1）/start/index.php
+++ fullstack-webdev/050_PHP基礎Part3/1000_クラス継承（Part1）/start/index.php
@@ -1,12 +1,15 @@
 <?php
+
+use Person as GlobalPerson;
+
 /**
  * クラス継承
  */
 class Person
 {
-    private $name;
+    protected $name;
     public $age;
-    public const WHERE = 'Earth';
+    public static $WHERE = 'Earth';
 
     function __construct($name, $age)
     {
@@ -24,9 +27,33 @@ class Person
     }
 }
 
-$bob = new Person('Bob', 18);
-$bob->hello();
-// $bob->hello()->bye();
+class Japanese extends Person {
+    
+    // public static $WHERE = '日本';
+
+    function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = '30';
+    }
+    
+    function hello() {
+        echo 'こんにちは、' . $this->name;
+        return $this;
+    }
+
+    function jyusho() {
+        echo '住所は' . static::$WHERE .'です。';
+        return $this;
+    }
+}
+
+$taro = new Japanese('タロウ', 18);
+$taro->hello();
+$taro->jyusho();
+echo $taro->age;
+
+// $taro->hello()->bye();
 
 // $tim = new Person('Tim', 32);
 // $tim->hello();
