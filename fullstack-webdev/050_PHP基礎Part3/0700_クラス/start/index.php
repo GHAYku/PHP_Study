d7c803c534484a5754c6431e953ab8251afea467
GHAYku <kusa07122@gmail.com>
2022-01-16 20:01:29 +0900

    [Finish]クラス

--- fullstack-webdev/050_PHP基礎Part3/0700_クラス/start/index.php
+++ fullstack-webdev/050_PHP基礎Part3/0700_クラス/start/index.php
@@ -1,10 +1,23 @@
-<?php 
+<?php
+
+use Person as GlobalPerson;
+
 class Person
 {
-    private $name;
+    public $name;
     public $age;
+    
+    function __construct($name,$age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
 
     function hello() {
         echo 'hello, ' . $this->name;
     }
 }
+
+$bob = new Person('Bob',18);
+$bob->hello();
