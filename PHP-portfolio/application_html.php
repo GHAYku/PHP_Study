ed88581cb3ce874e2833ecd28114f37a42176989
GHAYku <kusa07122@gmail.com>
2022-01-29 20:17:41 +0900

    [Finish]トップページ

--- PHP-portfolio/application_html.php
+++ PHP-portfolio/application_html.php
@@ -4,11 +4,14 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/earlyaccess/hannari.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Sawarabi+Mincho" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Fuzzy+Bubbles&family=Klee+One&family=Yuji+Boku&family=Yuji+Mai&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="style.css"/>
   <title>皆の失敗談</title>
 </head>
 <body>
-<header>
+  <header>
    <div class="container_logout">
     <div class="logo_area">
      <a href="top.php">
@@ -17,5 +20,11 @@
     </div>
    </div>
   </header>
+  <footer>
+    <div class="container">
+     <div class="gest-login">
+     </div>
+    </div>
+   </footer>
 </body>
 </html>
--- PHP-portfolio/top.php
+++ PHP-portfolio/top.php
@@ -5,30 +5,28 @@
 <main class="top_container">
  <div class="home_container">
   <div class="top_about">
-   <%= image_tag "BAKUSHOW!!_logo.png", size: "300x75",class: "log" %>
-    <h1 class="top_title">BAKUSHOW!!で爆笑。</h1>
+    <img class="log" src="LOGO.PNG" width="300" height="75">
+    <h1 class="top_title">失敗は成功の母。</h1>
     <ul>
-	    <li class="top_list">面白いと思ったことを投稿しよう。</li>
-	    <li class="top_list">大喜利にボケて更に面白くしよう。</li>
-	    <li class="top_list">面白い人をフォローしよう。</li>
-	    <li class="top_list">笑いを見せ合って輪を広げよう。</li>
+	    <li class="top_list">自分の失敗を投稿しよう。</li>
+	    <li class="top_list">失敗した人を応援しよう。</li>
+	    <li class="top_list">皆で励ましあおう。</li>
+	    <li class="top_list">成功に向かって突き進もう。</li>
 	   </ul>
-  </section>
+</div>
   <div class="top_login">
    <div class="other_login">
-    <h1>BAKUSHOW!!</h1>
-    <%= link_to(new_end_user_session_path, class: "btn-BAKUSHOW") do %>
-      <%= image_tag "icon2.png", size: "45x45"%>
-				  <span>
-				   ログイン
-				  </span>
-				<% end %>
-    <%= link_to(new_end_user_registration_path, class: "btn-BAKUSHOW") do %>
-    <%= image_tag "icon.png", size: "45x45"%>
-				  <span>
-				   新規アカウント作成
-				  </span>
-				<% end %>
+    <h1>一先ず参加する。</h1>
+      <a class="btn-BAKUSHOW" href="login.php" >
+				<span>
+				  ログイン
+				</span>
+      </a>
+      <a class="btn-BAKUSHOW" href="registration.php" >
+				<span>
+				  新規アカウント作成
+				</span>
+      </a>
 	 </div>
   </div>
   <div class="site_scription">
