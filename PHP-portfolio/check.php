0816cea0eafe1376e12b2969287dca0e4aea593e
GHAYku <kusa07122@gmail.com>
2022-02-03 21:35:42 +0900

    [Finish]新規登録画面

---
+++ PHP-portfolio/check.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+require('application_html.php');
+var_dump($_SESSION['form']);
+?>
+
+
+<main class="login">
+ <div class="form_container">
+  <div class="form_grop">
+   <h2>アカウント作成</h2>
+
+  </div>
+ </div>
+</main>
--- PHP-portfolio/registration.php
+++ PHP-portfolio/registration.php
@@ -1,10 +1,39 @@
 <?php
+  session_start();
   require('application_html.php');
-  $form = [];
+  $form = [
+    'name' => '',
+    'email' =>'',
+    'password'=>''
+  ];
   $error = [];
-  $form['name'] = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
-  if ($form['name'] === ''){
-    $error['name'] = 'blank';
+  /*フォームの内容をチェックする */
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $form['name'] = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+    if ($form['name'] === ''){
+      $error['name'] = 'blank';
+    }
+    $form['email'] = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+    if ($form['email'] === ''){
+      $error['email'] = 'blank';
+    }
+    $form['password'] = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+    if ($form['password'] === ''){
+      $error['password'] = 'blank';
+    } else if(strlen($form['password']) < 4){
+      $error['password'] = 'length';
+    }
+
+    if(empty($error)){
+      $_SESSION['form'] = $form;
+      header('Location: check.php');
+      exit();
+    }
+  }
+
+  /*htmlspecialcharsを短くする */
+  function h($value){
+    return htmlspecialchars($value, ENT_QUOTES);
   }
 ?>
 
@@ -14,15 +43,24 @@
    <h2>アカウント作成</h2>
    <form action="" method="post" enctype="multipart/form-data">
     <dl>
-      <input class= "form" type="text" name="name" size="35" maxlength="255" value="<?php echo htmlspecialchars($form['name'], ENT_QUOTES); ?>"/>
+      <input class= "form" type="text" name="name" size="35" maxlength="255" value="<?php echo h($form['name']); ?>"/>
       <?php if(isset($error['name']) && $error['name'] === 'blank'): ?>
        <p class="error">* ニックネームを入力してください</p>
       <?php endif; ?>
-      <input class= "form" type="text" name="email" size="35" maxlength="255" value=""/>
-      <p class="error">* メールアドレスを入力してください</p>
-      <input class= "form" type="password" name="password" size="10" maxlength="20" value=""/>
-      <p class="error">* パスワードを入力してください</p>
-      <p class="error">* パスワードは4文字以上で入力してください</p>
+
+      <input class= "form" type="text" name="email" size="35" maxlength="255" value="<?php echo h($form['email']); ?>"/>
+      <?php if(isset($error['email']) && $error['email'] === 'blank'): ?>
+       <p class="error">* メールアドレスを入力してください</p>
+      <?php endif ?>
+      <p class="error">* 指定されたメールアドレスはすでに登録されています。</p>
+
+      <input class= "form" type="password" name="password" size="10" maxlength="20" value="<?php echo h($form['password']); ?>"/>
+      <?php if(isset($error['password']) && $error['password'] === 'blank'): ?>
+       <p class="error">* パスワードを入力してください</p>
+      <?php endif ?>
+      <?php if(isset($error['password']) && $error['password'] === 'length'): ?>
+       <p class="error">* パスワードは4文字以上で入力してください</p>
+      <?php endif ?>
     </dl>
     <div class="actions">
       <input class="form_btn" type="submit" value="入力内容を確認する"/>
