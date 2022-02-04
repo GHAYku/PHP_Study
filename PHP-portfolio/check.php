81efb2eb165657c3986cace11f2c04a0d6522a3c
GHAYku <kusa07122@gmail.com>
2022-02-04 21:22:28 +0900

    [Fix]新規登録画面メールアドレス重複制御

--- PHP-portfolio/check.php
+++ PHP-portfolio/check.php
@@ -1,15 +1,50 @@
 <?php
 session_start();
 require('application_html.php');
-var_dump($_SESSION['form']);
+require('library.php');
+if (isset($_SESSION['form'])) {
+  $form = $_SESSION['form'];
+} else {
+  header('Location: registration.php');
+  exit();
+}
+
+$form['picture'] = '';
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $db = dbconnect();
+  $stmt = $db->prepare('insert into users(name,email,password,picture) VALUES(?,?,?,?)');
+  if (!$stmt) {
+    die($db->error);
+  }
+  $password = password_hash($form['password'],PASSWORD_DEFAULT);
+  $stmt->bind_param('ssss', $form['name'], $form['email'],$password,$form['picture']);
+  $success = $stmt->execute();
+  if (!$success) {
+    die($db->error);
+  }
+
+  unset($_SESSION['form']);
+  header('Location: mypage.php');
+}
+
 ?>
 
 
 <main class="login">
- <div class="form_container">
-  <div class="form_grop">
-   <h2>アカウント作成</h2>
-
+  <div class="form_container">
+    <div class="form_grop">
+      <h2>確認</h2>
+      <form action="" method="post">
+        <p>ニックネーム:<?php echo h($form['name']) ?></p>
+        <p>メールアドレス:<?php echo h($form['email']) ?></p>
+        <p>パスワード:表示されません</p>
+        <div class="actions">
+          <input class="form_btn" type="submit" value="この内容で登録する" />
+          <a href="registration.php?action=rewrite">書き直す</a>
+        </div>
+      </form>
+    </div>
   </div>
- </div>
 </main>
--- PHP-portfolio/registration.php
+++ PHP-portfolio/registration.php
@@ -1,11 +1,18 @@
 <?php
   session_start();
   require('application_html.php');
-  $form = [
-    'name' => '',
-    'email' =>'',
-    'password'=>''
-  ];
+  require('library.php');
+  if(isset($_GET['action']) && $_GET['action'] === 'rewrite' && isset($_SESSION['form'])){
+    $form = $_SESSION['form'];
+  } else {
+    $form = [
+      'name' => '',
+      'email' =>'',
+      'password'=>''
+    ];
+  }
+
+
   $error = [];
   /*フォームの内容をチェックする */
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -16,6 +23,23 @@
     $form['email'] = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
     if ($form['email'] === ''){
       $error['email'] = 'blank';
+    } else {
+      $db = dbconnect();
+      $stmt = $db->prepare('select count(*) from users where email=?');
+      if(!$stmt){
+        die($db->error);
+      }
+      $stmt->bind_param('s',$form['email']);
+      $success = $stmt->execute();
+      if(!$success) {
+        die($db->error);
+      }
+      $stmt->bind_result($cnt);
+      $stmt->fetch();
+
+      if($cnt > 0){
+        $error['email'] = 'duplicate';
+      }
     }
     $form['password'] = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
     if ($form['password'] === ''){
@@ -31,17 +55,13 @@
     }
   }
 
-  /*htmlspecialcharsを短くする */
-  function h($value){
-    return htmlspecialchars($value, ENT_QUOTES);
-  }
 ?>
 
 <main class="login">
  <div class="form_container">
   <div class="form_grop">
    <h2>アカウント作成</h2>
-   <form action="" method="post" enctype="multipart/form-data">
+   <form action="" method="post">
     <dl>
       <input class= "form" type="text" name="name" size="35" maxlength="255" value="<?php echo h($form['name']); ?>"/>
       <?php if(isset($error['name']) && $error['name'] === 'blank'): ?>
@@ -52,8 +72,9 @@
       <?php if(isset($error['email']) && $error['email'] === 'blank'): ?>
        <p class="error">* メールアドレスを入力してください</p>
       <?php endif ?>
-      <p class="error">* 指定されたメールアドレスはすでに登録されています。</p>
-
+      <?php if(isset($error['email']) && $error['email'] === 'duplicate'): ?>
+       <p class="error">* 指定されたメールアドレスはすでに登録されています。</p>
+      <?php endif ?>
       <input class= "form" type="password" name="password" size="10" maxlength="20" value="<?php echo h($form['password']); ?>"/>
       <?php if(isset($error['password']) && $error['password'] === 'blank'): ?>
        <p class="error">* パスワードを入力してください</p>
